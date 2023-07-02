91f5887830a5c7101223f5f3a00eaba5cc13eda9
nurmuhass <nurmuhass@gmail.com>
2023-07-02 18:18:21 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -54,9 +54,9 @@ const about = () => {
         <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light'>About Me</h2>
             <p className='font-medium'>
-            Hi, I'm Nurmuhass, a web developer and Mobile App developer with a passion for creating Fullstack beautiful, functional, 
+            Hi, I am Nurmuhass, a web developer and Mobile App developer with a passion for creating Fullstack beautiful, functional, 
 and user-centered digital experiences. With 5 years of experience in the field. I am always looking for 
-new and innovative ways to bring my clients' visions to life.
+new and innovative ways to bring my clients visions to life.
             </p>
 
             <p className='font-medium '>
@@ -64,7 +64,7 @@ new and innovative ways to bring my clients' visions to life.
             </p>
 
             <p className='font-medium my-4'>
-            Whether I'm working on a website, mobile app, or 
+            Whether I am working on a website, mobile app, or 
 other digital product, I bring my commitment to design excellence and user-centered thinking to 
 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
             </p>
